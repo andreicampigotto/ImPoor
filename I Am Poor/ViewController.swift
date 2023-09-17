756dd730c40e57627f563823828848025b79a1da
Andrei Campigotto <andreicampigotto@Andreis-MacBook-Pro.local>
2023-09-17 17:38:03 -0300

    commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Andrei Campigotto on 2023-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
